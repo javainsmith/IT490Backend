3ead4f8f0603ed65fb0475f608af775840499244
Javain Smith <jds66@njit.edu>
2018-10-18 14:09:28 -0400

    Error logger is working.

--- api.php
+++ api.php
@@ -3,7 +3,7 @@
 require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
-
+error_reporting(E_ALL);
 function foodLookup($data)
 {
 	$URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" . $data;
@@ -14,6 +14,50 @@ function foodLookup($data)
 	return $json;
 }
 
+function sendError($errno, $errstr, $errfile, $errline) {
+
+	echo $errstr;
+	switch ($errno) {
+	case E_ERROR:
+	case E_PARSE:
+	case E_CORE_ERROR:
+	case E_CORE_WARNING:
+	case E_COMPILE_ERROR:
+	case E_COMPILE_WARNING:
+	case E_WARNING:
+	case E_RECOVERABLE_ERROR:
+	case E_NOTICE:
+	case E_DEPRECATED:
+	case E_USER_DEPRECATED:
+	case E_STRICT:
+        case E_USER_NOTICE:
+	case E_USER_WARNING:
+	case E_USER_ERROR:
+	case E_ALL:
+             $client = new rabbitMQClient("testRabbitMQ.ini","errorServer");
+             $request = array();
+             $request['type'] = "error";
+             $request['errorString'] = $errstr;
+	     $request['errorFile'] = $errfile;
+	     $request['errorLine'] = $errline;
+	     $response = $client->send_request($request);
+             exit();	     
+	     break;
+	default:
+	     $client = new rabbitMQClient("testRabbitMQ.ini","errorServer");
+             $request = array();
+             $request['type'] = "error";
+             $request['errorString'] = $errstr;
+             $request['errorFile'] = $errfile;
+             $request['errorLine'] = $errline;
+	     $response = $client->send_request($request);
+	     exit();
+             break;
+
+    }
+}
+ 
+set_error_handler("sendError");
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
